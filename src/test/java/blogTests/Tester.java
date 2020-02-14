9528794d5e97012886bfdfaab8d4eb074a29cb97
DucklingLover <julian.heil.2000.79@gmail.com>
2020-02-14 10:27:44 +0100

    Preparation for JUnit Tests

---
+++ src/test/java/blogTests/Tester.java
@@ -0,0 +1,20 @@
+package blogTests;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author HEILJulian
+ */
+public class Tester {
+
+    public Tester() {
+
+    }
+
+    @Test
+    public void tester() {
+        System.out.println("ToDo");
+    }
+
+}
