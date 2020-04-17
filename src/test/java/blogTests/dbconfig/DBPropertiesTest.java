7835c355e1ade0f5d24ce74ccb1037206fe0fedd
DucklingLover <julian.heil.2000.79@gmail.com>
2020-04-17 10:31:15 +0200

    Added new unit tests

---
+++ src/test/java/blogTests/dbconfig/DBPropertiesTest.java
@@ -0,0 +1,21 @@
+package blogTests.dbconfig;
+
+import dbconfig.DBProperties;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lukas Patter
+ */
+public class DBPropertiesTest {
+
+    @Test
+    public void testDBProperties() {
+        System.out.println("*+Test Properties von Lukas Patter **");
+        assertEquals("DRIVER:", DBProperties.DB_DRIVER.getText(), DBProperties_Test.DRIVER);
+        assertEquals("URL:", DBProperties.DB_URL.getText(), DBProperties_Test.URL);
+        assertEquals("USERNAME:", DBProperties.DB_USERNAME.getText(), DBProperties_Test.USERNAME);
+        assertEquals("PASSWORD:", DBProperties.DB_PASSWORD.getText(), DBProperties_Test.PASSWORD);
+    }
+}
---
+++ src/test/java/blogTests/dbconfig/DBProperties_Test.java
@@ -0,0 +1,13 @@
+package blogTests.dbconfig;
+
+/**
+ *
+ * @author Lukas Patter
+ */
+public class DBProperties_Test {
+
+    public static final String DRIVER = "org.postgresql.Driver";
+    public static final String URL = "jdbc:postgresql://127.0.0.1:5432/PartyBlog";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "jJ18102000";
+}
