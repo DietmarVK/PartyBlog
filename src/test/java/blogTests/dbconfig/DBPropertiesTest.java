7a144b9301719d2cdd5623e7203bf0c523c04e31
DietmarVK <vk@htl-kaindorf.ac.at>
2020-04-17 14:20:29 +0200

    Update DBPropertiesTest.java
    neue Tests fail u pass

--- src/test/java/blogTests/dbconfig/DBPropertiesTest.java
+++ src/test/java/blogTests/dbconfig/DBPropertiesTest.java
@@ -13,9 +13,20 @@ public class DBPropertiesTest {
     @Test
     public void testDBProperties() {
         System.out.println("*+Test Properties von Lukas Patter **");
-        assertEquals("DRIVER:", DBProperties.DB_DRIVER.getText(), DBProperties_Test.DRIVER);
+        assertEquals("DRIVER:", DBProperties.DB_DRIVER.getText(), 
+                DBProperties_Test.DRIVER);
         assertEquals("URL:", DBProperties.DB_URL.getText(), DBProperties_Test.URL);
         assertEquals("USERNAME:", DBProperties.DB_USERNAME.getText(), DBProperties_Test.USERNAME);
         assertEquals("PASSWORD:", DBProperties.DB_PASSWORD.getText(), DBProperties_Test.PASSWORD);
     }
+    @Test
+    public void testDBPropertiesFail() {
+        System.out.println("*+Test Fail **");
+        assertEquals("DRIVER:", "XX","YY");
+    }
+    @Test
+    public void testDBPropertiesPass() {
+        System.out.println("*+Test Pass **");
+        assertEquals("DRIVER:", "XX","XX");
+    }    
 }
