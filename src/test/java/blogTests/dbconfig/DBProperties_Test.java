dbb80339613905a4a7b0068f7b2a52726e4d1330
DucklingLover <43092476+DucklingLover@users.noreply.github.com>
2020-04-17 11:27:40 +0200

    Update DBProperties_Test.java

--- src/test/java/blogTests/dbconfig/DBProperties_Test.java
+++ src/test/java/blogTests/dbconfig/DBProperties_Test.java
@@ -7,7 +7,7 @@ package blogTests.dbconfig;
 public class DBProperties_Test {
 
     public static final String DRIVER = "org.postgresql.Driver";
-    public static final String URL = "jdbc:postgresql://127.0.0.1:5432/PartyBlog";
+    public static final String URL = "jdbc:postgresql://127.0.0.1:5432/PartyBlog2";
     public static final String USERNAME = "postgres";
     public static final String PASSWORD = "jJ18102000";
 }
